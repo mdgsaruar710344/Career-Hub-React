2964a551c08b308f9ab2928325e4d04ba7766cea
mdgsaruar710344 <golamsaruar004@gmail.com>
2024-11-04 17:23:18 +0600

    added filter menu from daisyUI

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -3,40 +3,50 @@ import { getStoredApplication } from "../../Utilities/localStorage";
 import AppliedJob from "../AppliedJob/AppliedJob";
 
 const AppliedJobs = () => {
- 
-  const [appliedjobs,setAppliedJobs]=useState([]);
+
+  const [appliedjobs, setAppliedJobs] = useState([]);
 
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  
-      fetch('jobs.json')
-      .then(res=>res.json())
-      .then(jobs=>{
-        if(jobs.length>0){
-          let matchingJobarray=[];
-          const jobsfromLS= getStoredApplication();
-          for(const id of jobsfromLS){
-          const matchedJob=  jobs.find(job=>job.id==id);
-         matchingJobarray.push(matchedJob);
-           
+
+    fetch('jobs.json')
+      .then(res => res.json())
+      .then(jobs => {
+        if (jobs.length > 0) {
+          let matchingJobarray = [];
+          const jobsfromLS = getStoredApplication();
+          for (const id of jobsfromLS) {
+            const matchedJob = jobs.find(job => job.id == id);
+            matchingJobarray.push(matchedJob);
+
           }
           setAppliedJobs(matchingJobarray);
           console.log(matchingJobarray);
         }
 
       })
-   
-    
-},[]);
 
 
-    
+  }, []);
+
+
+
 
   return (
     <div>
       Applied are here:
-      {appliedjobs? appliedjobs.map(job=><AppliedJob key={job.id} job={job}></AppliedJob>):'No jobs'}
+      <div>
+        <details className="dropdown">
+          <summary className="btn m-1">Filter</summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+            <li><a>All</a></li>
+            <li><a>Remote</a></li>
+            <li><a>Onsite</a></li>
+          </ul>
+        </details>
+      </div>
+      {appliedjobs ? appliedjobs.map(job => <AppliedJob key={job.id} job={job}></AppliedJob>) : 'No jobs'}
     </div>
   );
 };
